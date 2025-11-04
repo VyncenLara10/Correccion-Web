f8719c6540e95c48cc7101798885b401af85ad22
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2025-11-04 11:11:22 -0600

    fix: settings update with cors

--- backend/TikalInvest/settings.py
+++ backend/TikalInvest/settings.py
@@ -164,6 +164,30 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
     'https://front.g1.atenea.lat',
